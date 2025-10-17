f679ea90619bfc571a47e9b6ca8715b15581c7a6
Ronny Sotillet <ronny.sotillet777@gmail.com>
2025-10-17 15:47:58 -0500

    add MONGDB_URI

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,4 +1,4 @@
-import {connect, connection} from 'mongoose'
+import { connect, connection } from 'mongoose'
 
 const conn = {
     isConnected: false
@@ -7,17 +7,18 @@ const conn = {
 export async function connectDB() {
     console.log('Connecting...')
     if (conn.isConnected) return;
-
-    const db = await connect('mongodb://localhost/melitest')
+    const MONGODB_URI = process.env.MONGODB_URI;
+    if (!MONGODB_URI) throw new Error('MONGODB_URI no está definido')
+    const db = await connect(MONGODB_URI!)
     console.log(db.connection.db?.databaseName)
 
     conn.isConnected = !!db.connections[0].readyState
 }
 
-connection.on('connected',()=>{
+connection.on('connected', () => {
     console.log('Mongoose is connected')
 })
 
-connection.on('error',(err)=>{
+connection.on('error', (err) => {
     console.log('Mongoose connection error', err)
 })
